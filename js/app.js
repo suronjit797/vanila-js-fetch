99f3112b436ec933544b0bb0a03284be703ce7f5
suronjit797 <suronjit797@gmail.com>
2022-02-28 20:52:39 +0600

    header section

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-dfs
+document.getElementById('searchForm').addEventListener('submit', (event) => {
+    event.preventDefault()
+    let inputText = document.querySelector('#searchForm input')
+    let inputTextValue = inputText.value
+    console.log(inputTextValue)
+})
