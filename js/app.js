f1f9b4f11b1ccc183c0c822b5c1101a24a479a17
suronjit797 <suronjit797@gmail.com>
2022-03-01 01:58:02 +0600

    formate

--- js/app.js
+++ js/app.js
@@ -1,143 +1,139 @@
 // spinner
-let spinner = document.querySelector('.spinner-border')
-let showMore = document.querySelector('.show_more')
+let spinner = document.querySelector(".spinner-border");
+let showMore = document.querySelector(".show_more");
 
-let maxResult = 20
-// onsubmit 
+let maxResult = 20;
+// onsubmit
 
-document.getElementById('searchForm').addEventListener('submit', (event) => {
-    event.preventDefault()
-    let inputText = document.querySelector('#searchForm input')
-    let inputTextValue = (inputText.value).toLowerCase()
-    if (inputTextValue) {
-        fetchMobileData(inputTextValue)
-        spinner.classList.remove('d-none')
-    } else {
-        errorResult('please fill input box to search something')
-    }
-    inputText.value = ''
-})
-
-
-// fetch all data from search input 
-const fetchMobileData = searchText => {
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
-        .then(res => res.json())
-        .then(data => showMobileData(data.data))
-        .catch(err => console.log(err))
-}
-// show search result
-const showMobileData = data => {
-    // spinner
-    spinner.classList.add('d-none')
-    // result div
-    let resultSection = document.getElementById('resultSection')
-    resultSection.innerHTML = ''
+document.getElementById("searchForm").addEventListener("submit", (event) => {
+    event.preventDefault();
     // error div
-    document.getElementById('errorBox').innerHTML = ''
+    document.getElementById("errorBox").innerHTML = "";
+    let inputText = document.querySelector("#searchForm input");
+    let inputTextValue = inputText.value.toLowerCase();
+    if (inputTextValue) {
+        fetchMobileData(inputTextValue);
+        spinner.classList.remove("d-none");
+    } else {
+        errorResult("please fill input box to search something");
+    }
+    inputText.value = "";
+});
+
+// fetch all data from search input
+const fetchMobileData = (searchText) => {
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+        .then((res) => res.json())
+        .then((data) => showMobileData(data.data))
+        .catch((err) => console.log(err));
+};
+// show search result
+const showMobileData = (data) => {
+    // spinner
+    spinner.classList.add("d-none");
+    // result div
+    let resultSection = document.getElementById("resultSection");
+    resultSection.innerHTML = "";
+
 
     // if search has data
     if (data.length > 0) {
         data.forEach((phone, index) => {
             if (index < maxResult) {
-                let createDiv = document.createElement('div')
-                createDiv.classList.add('col')
+                let createDiv = document.createElement("div");
+                createDiv.classList.add("col");
                 createDiv.innerHTML = `
                 <div class="card h-100">
                     <img src="${phone.image}" class="card-img-top w-75 mx-auto my-4" alt="...">
                     <div class="card-body d-flex flex-column">
                         <h5 class="card-title"> ${phone.phone_name} </h5>
-                        <p onclick="searchBrand('${(phone.brand).toLowerCase()}')">brand: <span class="text-primary brand_name"> ${phone.brand} </span></p>
+                        <p onclick="searchBrand('${phone.brand.toLowerCase()}')">brand: <span class="text-primary brand_name"> ${phone.brand} </span></p>
                         <div class="mt-auto">
                             <button onclick="mobileExplore('${phone.slug}')" class="mt-auto btn btn-primary"> Explore More </button>
                         <div/>
                     </div>
-                </div>`
+                </div>`;
 
-                resultSection.appendChild(createDiv)
+                resultSection.appendChild(createDiv);
             }
         });
     } else {
-        errorResult('no data found')
+        errorResult("no data found");
     }
 
     if (data.length > maxResult) {
-        showMore.classList.remove('d-none')
+        showMore.classList.remove("d-none");
 
         // show more function
-        showMore.addEventListener('click', () => {
-            maxResult = data.length
-            showMore.classList.add('d-none')
+        showMore.addEventListener("click", () => {
+            maxResult = data.length;
+            showMore.classList.add("d-none");
             // result div
-            let resultSection = document.getElementById('resultSection')
-            resultSection.innerHTML = ''
+            let resultSection = document.getElementById("resultSection");
+            resultSection.innerHTML = "";
             data.forEach((phone, index) => {
                 if (index < maxResult) {
-                    let createDiv = document.createElement('div')
-                    createDiv.classList.add('col')
+                    let createDiv = document.createElement("div");
+                    createDiv.classList.add("col");
                     createDiv.innerHTML = `
                     <div class="card h-100">
                         <img src="${phone.image}" class="card-img-top w-75 mx-auto my-4" alt="...">
                         <div class="card-body d-flex flex-column">
                             <h5 class="card-title"> ${phone.phone_name} </h5>
-                            <p onclick="searchBrand('${(phone.brand).toLowerCase()}')">brand: <span class="text-primary brand_name"> ${phone.brand} </span></p>
+                            <p onclick="searchBrand('${phone.brand.toLowerCase()}')">brand: <span class="text-primary brand_name"> ${phone.brand} </span></p>
                             <div class="mt-auto">
                                 <button onclick="mobileExplore('${phone.slug}')" class="mt-auto btn btn-primary"> Explore More </button>
                             <div/>
                         </div>
-                    </div>`
+                    </div>`;
 
-                    resultSection.appendChild(createDiv)
+                    resultSection.appendChild(createDiv);
                 }
             });
-        })
-
+        });
     }
-}
+};
 
 // search by brand
-let searchBrand = brandName => {
+let searchBrand = (brandName) => {
     // result div
-    let resultSection = document.getElementById('resultSection')
-    resultSection.innerHTML = ''
+    let resultSection = document.getElementById("resultSection");
+    resultSection.innerHTML = "";
     // spinner
-    spinner.classList.remove('d-none')
-    document.querySelector('#searchForm input').value = brandName
-    fetchMobileData(brandName)
-}
+    spinner.classList.remove("d-none");
+    document.querySelector("#searchForm input").value = brandName;
+    fetchMobileData(brandName);
+};
 
 // show search result
-const errorResult = message => {
-    spinner.classList.add('d-none')
-    let errorBox = document.getElementById('errorBox')
-    errorBox.innerHTML = ''
-    document.getElementById('resultSection').innerHTML = ''
-    let createDiv = document.createElement('div')
-    createDiv.classList.add('text-danger', 'text-capitalize')
-    createDiv.innerText = message
-    errorBox.appendChild(createDiv)
-}
-
-
+const errorResult = (message) => {
+    spinner.classList.add("d-none");
+    let errorBox = document.getElementById("errorBox");
+    errorBox.innerHTML = "";
+    document.getElementById("resultSection").innerHTML = "";
+    let createDiv = document.createElement("div");
+    createDiv.classList.add("text-danger", "text-capitalize");
+    createDiv.innerText = message;
+    errorBox.appendChild(createDiv);
+};
 
 // modal for single data
-const mobileExplore = id => {
+const mobileExplore = (id) => {
     fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
-        .then(res => res.json())
-        .then(data => showSingleData(data.data))
+        .then((res) => res.json())
+        .then((data) => showSingleData(data.data));
     // .catch(err=>console.log(err))
-}
+};
 
-
-const showModal = document.querySelector('.showSingleData')
-// single data function 
-const showSingleData = data => {
-    document.body.style.overflowY = 'hidden'
-    showModal.classList.remove('d-none')
-    showModal.innerHTML = ''
-    let cardDiv = document.createElement('div')
-    cardDiv.onclick = () => singleCard(event)
-    cardDiv.classList.add('card')
+const showModal = document.querySelector(".showSingleData");
+// single data function
+const showSingleData = (data) => {
+    document.body.style.overflowY = "hidden";
+    showModal.classList.remove("d-none");
+    showModal.innerHTML = "";
+    let cardDiv = document.createElement("div");
+    cardDiv.onclick = () => singleCard(event);
+    cardDiv.classList.add("card");
     cardDiv.innerHTML = `
             <div class="close fw-bold" onclick="closeModel()" >X</div>
             <img src="${data.image ? data.image : ""}" class="card-img-top mb-3" alt="...">
@@ -155,7 +151,7 @@ const showSingleData = data => {
                     <li>
                         <span> Sensors </span>
                         <span>
-                            ${(data.mainFeatures.sensors.map(item => "<span> " + item + " </span>"))}
+                            ${data.mainFeatures.sensors.map((item) => "<span> " + item + " </span>")}
                         </span>
                     </li>
                 </ul>
@@ -171,21 +167,20 @@ const showSingleData = data => {
             </div>
 
 
-        `
-    showModal.appendChild(cardDiv)
+        `;
+    showModal.appendChild(cardDiv);
 
-
-    console.log(data)
-}
+    console.log(data);
+};
 
 let closeModel = () => {
-    document.body.style.overflowY = 'visible'
-    showModal.classList.add('d-none')
-}
+    document.body.style.overflowY = "visible";
+    showModal.classList.add("d-none");
+};
 
-showModal.addEventListener('click', closeModel)
+showModal.addEventListener("click", closeModel);
 
 // to stop bubbling
 let singleCard = (event) => {
-    event.stopPropagation()
-}
+    event.stopPropagation();
+};
